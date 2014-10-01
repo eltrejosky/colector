a996a2fd1cf2f1adf231d46f44e64e7c77153aba
eltrejosky <king_greenday@hotmail.com>
2014-10-01 11:07:48 -0600

    Subida del proyecto

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Practica I - Computo II</title>
+    <meta charset="utf-8">    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <meta name = "format-detection" content = "telephone=no" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">
+    <link rel="stylesheet" href="css/bootstrap.css" >
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/camera.css">
+	<script src="js/jquery.js"></script>
+    <script src="js/jquery-migrate-1.2.1.js"></script>
+    <script src="js/superfish.js"></script>
+    <script src="js/jquery.mobilemenu.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.ui.totop.js"></script>
+    <script src="js/jquery.touchSwipe.min.js"></script>
+    <script src="js/jquery.equalheights.js"></script>
+</head>
+<body>
+<header id="header">
+      <div class="menuheader">
+          <div class="container">
+            <nav class="navbar navbar-default navbar-static-top tm_navbar" role="navigation">
+                <ul class="nav sf-menu">
+				     <li class="active"><a href="index.html">Inicio</a>
+                  </li>
+                  <li class="active"><a href="#">Computo 1</a>
+                    <ul>
+                      <li><img src="img/arrowup.png" alt="">
+					   <a href="practica1.html">Practica 1</a></li>
+                       <li><a href="practica2.html">Practica 2</a></li>
+                        <li><a href="practica3.html">Practica 3</a>
+					  <li><a class="last" href="parcial.html">Parcial</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li><a href="#">Computo 2</a>
+				   <ul>
+                      <li><img src="img/arrowup.png" alt="">
+					  <a target="_parent" href="criptografia.html">Criptografia Asimetrica</a></li>
+                      <li><a href="#">profile</a></li>
+                      <li><a class="last" href="oscomerce.html">news</a>
+                      </li>
+                    </ul>
+				  </li>
+                  <li><a href="#">Computo 3</a>
+				   <ul>
+                      <li><img src="img/arrowup.png" alt=""><a href="#">info</a></li>
+                      <li><a href="#">profile</a></li>
+                      <li><a class="last" href="#">news</a>
+                      </li>
+                    </ul>
+				  </li>
+            </nav>
+          </div>
+      </div>
+</header>
+<iframe src="pdf/practica2.pdf" style="width:100%; height:585.5px;" frameborder="0"></iframe>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/tm-scripts.js"></script>
+</body>
+</html>
